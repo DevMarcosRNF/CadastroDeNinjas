1153d69816dd3c1608142aa122fafdeece89278a
DevMarcosRNF <javadevmarcos@gmail.com>
2026-08-05 09:32:42 -0300

    Cria entidade ninja

---
+++ src/main/java/br/com/marcosrnf/cadastroDeNinjas/NinjaController.java
@@ -0,0 +1,15 @@
+package br.com.marcosrnf.cadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a tela de boas vindas! ";
+    }
+}
