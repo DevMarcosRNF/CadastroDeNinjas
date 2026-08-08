bb685a941d7b4bebaa046d27d442b7fd37e653a0
DevMarcosRNF <javadevmarcos@gmail.com>
2026-08-07 23:48:32 -0300

    Cria o mock de endpoint de missoes

--- src/main/java/br/com/marcosrnf/cadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/br/com/marcosrnf/cadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,33 @@
 package br.com.marcosrnf.cadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    @PostMapping("/criar")
+    public String criarMissao(@RequestBody String missao){
+        return "Missão criada: " + missao;
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Retornando lista de missões...";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarMissaoPorId(@PathVariable Long id){
+        return "Listando missao do ID: " + id;
+    }
+
+    @PutMapping("/editar/{id}")
+    public String editarMissao(@RequestBody String body, @PathVariable Long id){
+        return "Editando missão de ID: " + id + "( " + body + " )";
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletarMissao(@PathVariable Long id){
+        return "Deletando missão de ID: " + id;
+    }
 }
