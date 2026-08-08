8d551a5ea1c6322fc3d387a1fa797fc51422b93c
DevMarcosRNF <javadevmarcos@gmail.com>
2026-08-07 21:39:30 -0300

    feat: criação de mock de endpoints

--- src/main/java/br/com/marcosrnf/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/marcosrnf/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package br.com.marcosrnf.cadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,30 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é a tela de boas vindas! ";
+    }
+
+    @PostMapping("/criar")
+    public String criarNinja(@RequestBody String ninja){
+        return "Ninja criado com sucesso!" + ninja;
+    }
+
+    @GetMapping("/buscar-todos")
+    public String buscarTodosOsNinjas(){
+        return "Buscando todos os ninjas...";
+    }
+
+    @GetMapping("/buscar-por-id/{id}")
+    public String buscarNinjaPorId(@PathVariable Long id){
+        return "Buscando o ninja de ID " + id + "!";
+    }
+
+    @PutMapping("/editar/{id}")
+    public String editarNinja(@RequestBody String ninja, @PathVariable Long id){
+        return "Editando o ninja " + ninja + " de ID" + id;
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletarNinja(@PathVariable Long id){
+        return "Esse método será VOID futuramente. deletando ninja de ID " + id;
     }
 }
