394bb189fb85d853969547ff293ba2793550dd8f
DevMarcosRNF <javadevmarcos@gmail.com>
2026-08-08 23:56:09 -0300

    fix: Inclui coluna rank na entidade que foi gerada atraves da Migration V2

--- src/main/java/br/com/marcosrnf/cadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/br/com/marcosrnf/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -30,6 +30,9 @@ public class NinjaModel {
 
     @Column(name = "idade")
     private int idade;
+
+    @Column(name = "rank")
+    private String rank;
     // @ManyToOne - MUITOS ninjas para UMA missão.
     @ManyToOne
     @JoinColumn(name = "missoes_id")
