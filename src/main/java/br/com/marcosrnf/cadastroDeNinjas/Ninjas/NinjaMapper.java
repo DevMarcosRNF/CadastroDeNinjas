c1d10d5418853e036412cda3d1f832b6dc242672
DevMarcosRNF <javadevmarcos@gmail.com>
2026-08-08 23:58:01 -0300

    feat: cria mapper e dto do NinjaModel para seguranca da aplicacao

--- src/main/java/br/com/marcosrnf/cadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/br/com/marcosrnf/cadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,31 @@
 package br.com.marcosrnf.cadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDto){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDto.getId());
+        ninjaModel.setNome(ninjaDto.getNome());
+        ninjaModel.setIdade(ninjaDto.getIdade());
+        ninjaModel.setImgUrl(ninjaDto.getImgUrl());
+        ninjaModel.setEmail(ninjaDto.getEmail());
+        ninjaModel.setRank(ninjaDto.getRank());
+        ninjaModel.setMissoes(ninjaDto.getMissoes());
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDto = new NinjaDTO();
+        ninjaDto.setId(ninjaModel.getId());
+        ninjaDto.setNome(ninjaModel.getNome());
+        ninjaDto.setIdade(ninjaModel.getIdade());
+        ninjaDto.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDto.setEmail(ninjaModel.getEmail());
+        ninjaDto.setRank(ninjaModel.getRank());
+        ninjaDto.setMissoes(ninjaModel.getMissoes());
+        return ninjaDto;
+    }
 }
